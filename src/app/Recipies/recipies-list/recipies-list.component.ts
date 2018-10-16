9a3bb0c435ce2e3372c061255fa894aabec555e4
pns09 <s530715@nwmissouri.edu>
2018-10-16 14:28:04 -0500

    creating a project

--- src/app/Recipies/recipies-list/recipies-list.component.ts
+++ src/app/Recipies/recipies-list/recipies-list.component.ts
@@ -9,9 +9,7 @@ export class RecipiesListComponent implements OnInit {
 
 recipies: Recipies[] = [
 
-  new Recipies('A Test Recipie', "This is a description","https://www.google.com/search?q=recipe+images&rlz=1C1GCEA_enUS794US796&tbm=isch&source=iu&ictx=1&fir=O1EmZ2ihYKQUMM%253A%252Cf37DyeVQb8s3LM%252C_&usg=AI4_-kRceoZkn1yhPoqt4Z57aRtm6xXP8w&sa=X&ved=2ahUKEwiZ95P7ovvdAhWE44MKHfWEB4kQ9QEwDHoECAIQHA#imgrc=O1EmZ2ihYKQUMM:"),
-  
-
+  new Recipies('A Test Recipie', "This is a description","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC4lAwEbHCr5c-vCz-U15iM2Vns1840L1SdJ1BlXbMrGjAikKAng"),
 ];
 
 
--- src/app/shopping/shopping.component.ts
+++ src/app/shopping/shopping.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ingredient}from '../shared/ingredient.model'
 @Component({
   selector: 'app-shopping',
   templateUrl: './shopping.component.html',
   styleUrls: ['./shopping.component.css']
 })
 export class ShoppingComponent implements OnInit {
+ingredients: ingredient[]=[
+  new ingredient('apple',10),
+  new ingredient('banana',5),
 
+];
   constructor() { }
 
   ngOnInit() {
